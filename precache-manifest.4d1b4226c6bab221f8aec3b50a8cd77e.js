deb5e19f566514fbae3ad3775f7e45de62f2b4d3
SuperAiden <qpqp10212@gmail.com>
2019-10-17 00:36:10 +1100

    Updates

---
+++ precache-manifest.4d1b4226c6bab221f8aec3b50a8cd77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89059c49e9070cc9a0a84c7892f61d7a",
+    "url": "/salarycaculator/index.html"
+  },
+  {
+    "revision": "8706256ece4211adc4c3",
+    "url": "/salarycaculator/static/css/main.c47a2513.chunk.css"
+  },
+  {
+    "revision": "fd1f11d3e0c7a4a092b9",
+    "url": "/salarycaculator/static/js/2.9ceae214.chunk.js"
+  },
+  {
+    "revision": "8706256ece4211adc4c3",
+    "url": "/salarycaculator/static/js/main.c585dd7a.chunk.js"
+  },
+  {
+    "revision": "4c53ad919eedd48cf6c1",
+    "url": "/salarycaculator/static/js/runtime-main.77bf2603.js"
+  }
+]);
